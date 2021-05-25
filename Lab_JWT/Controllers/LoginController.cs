b0168718ff51f0aca766eae8c887dccd93d1877d
jyu <s123600g@gmail.com>
2021-05-25 22:56:42 +0800

    feat: Add test api about refresh token request process.

--- Lab_JWT/Controllers/LoginController.cs
+++ Lab_JWT/Controllers/LoginController.cs
@@ -121,5 +121,95 @@ namespace Lab_JWT.Controllers
 
             return res;
         }
+
+        [Route("api/refresh")]
+        [HttpPost]
+        [Authorize]
+        public async Task<Response> RefreshToken()
+        {
+            Response res = new Response();
+            string getOldToken_JTI = string.Empty;
+
+            try
+            {
+                #region 檢查目前Token是否有被鎖住可延續狀態
+
+                // 先取得目前Token內jyi
+                var getSouresToken = (
+                    from UC in User.Claims
+                    where UC.Type == "jti"
+                    select UC
+                ).FirstOrDefault();
+
+                getOldToken_JTI = getSouresToken.Value.ToString();
+
+                // 取的Token檢查鎖定狀態
+                bool checkStatuss = await jwtHelper.CheckTokenCanRefresh(
+                    getOldToken_JTI
+                );
+
+                // 如果回傳是True，代表該Token已經被鎖定，不能再給予延續換發
+                if (checkStatuss)
+                {
+                    res.Status = false;
+                    res.Msg = "該Token已經不能再延續換發新Token。";
+                    res.JwtToken = string.Empty;
+
+                    return res;
+                }
+
+                #endregion
+
+                #region  執行延續換發新Token
+
+                JWTCliam cliam = new JWTCliam();
+
+                // 產生Token過期時間
+                DateTime getExpireDateTime = DateTime.UtcNow.AddMinutes(jwtConfig.ExpireDateTime);
+
+                cliam.iss = jwtConfig.Issuer;
+                cliam.sub = Guid.NewGuid().ToString(); // 放User內容，這裡放自動產生Guid值
+                cliam.iat = DateTimeOffset.UtcNow.ToUnixTimeSeconds().ToString();
+                cliam.nbf = DateTimeOffset.UtcNow.ToUnixTimeSeconds().ToString();
+                // cliam.jti = Guid.NewGuid().ToString(); // Token 識別獨立ID，這裡不應該賦予，而是由JWT處理服務內給予
+                cliam.exp = getExpireDateTime.ToString();
+
+                // 執行產生JWT，取得回應結果
+                RunStatus getJWTResponse = await jwtHelper.RefreshToken(
+                    getOldToken_JTI,
+                    cliam,
+                    jwtConfig.SignKey,
+                    jwtConfig.Issuer,
+                    jwtConfig.ExpireDateTime
+                );
+
+                switch (getJWTResponse.isSuccess)
+                {
+                    case true:
+
+                        res.Status = true;
+                        res.JwtToken = getJWTResponse.jwt;
+                        res.Msg = "Done.";
+
+                        break;
+
+                    case false:
+
+                        res.Status = false;
+                        res.JwtToken = string.Empty;
+                        res.Msg = "驗證過程發生錯誤.";
+
+                        break;
+                }
+
+                #endregion
+            }
+            catch (Exception ex)
+            {
+                log.LogError($"{ex.Message}\n{ex.StackTrace}");
+            }
+
+            return res;
+        }
     }
 }
